52ad5d72b021e3171f0b774304385587fe10104d
Jun Jian Shen <jshen9085@gmail.com>
2019-01-01 16:21:40 -0500

    solved two more problem

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,19 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let arr = [...array]; // just create a copy and not change the original array
+  let containerArr = [];
+  while (arr.length > 0) {
+    containerArr.push(arr.slice(0, size));
+    arr.splice(0, size)
+  }
+  return containerArr;
+}
+
+function chunk(array, size) {
+  let chunked = [];
+  
+}
 
 module.exports = chunk;
